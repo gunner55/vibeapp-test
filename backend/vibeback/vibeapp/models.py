f8209bd62a4cdb505209ab0ee7bfe4847dfdacad
gunner55 <tamunoda@gmail.com>
2020-12-15 22:24:14 +0000

    vibeapp copy

---
+++ backend/vibeback/vibeapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+from django.db import models
